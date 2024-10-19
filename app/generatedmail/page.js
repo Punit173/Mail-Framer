58e85752d48b980cff63d65f99967020e3406b5f
Punit <jpunit2005@gmail.com>
2024-10-20 03:46:12 +0530

    Next page

--- app/generatedmail/page.js
+++ app/generatedmail/page.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-        
+    <div className='flex flex-col gap-5'>
+        <h1 className='text-4xl font-bold text-center'>Drafted <span className='text-yellow-300'>Mail</span></h1>
+      <div className='relative'>
+        <h1 className='absolute left-14 top-14 text-white text-4xl font-bold z-10'>
+          Hello
+        </h1>
+        <img
+          className='rotate-90 w-[5500px] md:w-[1200px] md:rotate-0'
+          src="https://images.freeimages.com/images/large-previews/f21/vintage-letter-paper-texture-0410-5699329.jpg?fmt=webp&w=500"
+          alt="imgnotfound"
+        />
+      </div>
     </div>
   )
 }
--- app/inputdesc/page.js
+++ app/inputdesc/page.js
@@ -3,10 +3,10 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <div className='flex flex-col gap-5'>
-        <h1 className='text-2xl font-medium'>No more boring <span className='text-yellow-400'>mails</span> just write down your content</h1>
-        <input className='h-56 rounded-xl p-5 text-black placeholder:flex' placeholder='Enter the description here...' type="text" name="" id="" />
-        <button className='rounded-xl p-3 bg-yellow-500 hover:bg-yellow-300 text-black font-bold'>Generate a beautiful mail</button>
+      <div className='flex flex-col gap-5 p-6'>
+        <h1 className='text-2xl font-medium text-center'>No more boring <span className='text-yellow-400'>mails</span> just write down your content</h1>
+        <input className='h-56 rounded-xl p-5 text-black ' placeholder='Enter the description here...' type="text" name="" id="" />
+        <button className='rounded-xl p-3 bg-yellow-400 hover:bg-yellow-300 text-black text-lg font-semibold tracking-widest'>Generate a beautiful mail</button>
       </div>
     </div>
   )
