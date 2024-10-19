8c20475453dede56d2d0d497e9cee583e3998956
Punit <jpunit2005@gmail.com>
2024-10-20 03:08:26 +0530

    Mail Framer

---
+++ app/home/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-2xl'>A place to write your mails in an effective way...</h1>
+    </div>
+  )
+}
+
+export default page
